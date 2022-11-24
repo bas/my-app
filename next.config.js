80b4054c8600332b19fd52cc6a5b33700e1629a6
Bas Peters <bas@github.com>
2022-11-24 18:02:38 +0100

    Add asset prefix

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   assetPrefix: isProd ? "/my-app/" : "",
+  publicRuntimeConfig: { 
+    assetPrefix: process.env.ASSET_PREFIX || '' 
+  },
   reactStrictMode: true,
   swcMinify: true,
   images: {

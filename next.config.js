f9d0327b85b7e00061a93ac34e5963205225bfea
Bas Peters <bas@github.com>
2022-11-24 17:44:12 +0100

    Add asset prefix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
+  assetPrefix: isProd ? "/my-app/" : "",
   reactStrictMode: true,
   swcMinify: true,
   images: {
     unoptimized: true
   },
-  assetPrefix: './my-app',
 }
 
 module.exports = nextConfig
